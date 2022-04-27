0af23b596223ab3799f4813e726b31f55ed6a4a1
Lasha Vachadze <vachadzelasha@gmail.com>
2022-04-27 15:27:01 +0400

    Visual studio1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Lasha Vachadze !");
+            string text=Console.ReadLine();
+            Console.WriteLine("Type Text:"+text);
+
+        }
+    }
+}
